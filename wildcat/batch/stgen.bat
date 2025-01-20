119478380fdd916ef3700367100d16b730bb4564
Bryan Wann <bryan.wann@tuxedocatbbs.com>
2025-01-19 22:37:47 -0800

    fix stgen to work with fossil or direct modem
    - quick check if we're running the USRDSF profile we're running
    a FOSSIL driver, otherwise assume direct to a modem and hope our
    IRQ env variable is set
    - fix gotos

--- wildcat/batch/stgen.bat
+++ wildcat/batch/stgen.bat
@@ -7,7 +7,7 @@ REM Needs ATS02=43 and S14=0 set on our modems so door can grab attention
 REM (and not hang up) and send AT commands to get USR statistics
 
 REM Not supported if we're a telnet client
-IF %WCMDM%==TELNET GOTO :TELNET
+IF %WCMDM%==TELNET GOTO TELNET
 
 REM clear up some env space, silly Windows
 SET WINDOWS_TRACING_LOGFILE=
@@ -22,11 +22,20 @@ SET STGTMP=C:\WILDCAT\WCWORK\NODE%WCNODEID%\
 
 cd \doors\stgen
 
+REM Detect if we're using a direct or fossil MDM profile
+IF %WCMDM%==USRDSF GOTO FOSSIL
+
 REM NFPORT is our NetFoss port number, write to stgen.log for debugging
 REM If we had Caller-id info we could pass it as the 2nd argument to
 REM STGEN-MC
 
+:SERIAL
+STGEN-MC %WCPORTID% %WCIRQID% %WCNODEID% 43 0 c:\doors\stgen\stgen.log /M3
+GOTO END
+
+:FOSSIL
 STGEN-MC %NFPORT% 0 %WCNODEID% 43 1 c:\doors\stgen\stgen.log /M3
+GOTO END
 
 :TELNET
 :END
